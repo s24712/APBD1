dec5b18e840af9091616993466181482f86b410a
s24712 <s24712@pjwstk.edu.pl>
2024-03-17 15:06:57 +0100

    Added method for avarage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-for (int i = 0; i < 3; i++)
+﻿namespace ConsoleApp1
 {
-    Console.WriteLine(i+1 + ". Hello, Git!");
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i + 1 + ". Hello, Git!");
+            }
+
+            int[] arr = new[] { 1, 3, 4, 5, 6 };
+
+            Console.WriteLine(srednia(arr));
+            
+        }
+
+        static int srednia(int[] arr)
+        {
+            int tmp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                tmp += arr[i];
+            }
+
+            return tmp / arr.Length;
+        }
+    }
 }
